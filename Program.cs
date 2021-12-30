4b5ca52f7a0ad684cfd429289aadff30257d566a
lowie <defermelowie@gmail.com>
2021-12-30 12:25:45 +0100

    Removed unnecessary code

--- Program.cs
+++ Program.cs
@@ -9,25 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IActivityService, ActivityService>();
 builder.Services.AddDbContext<ActivityContext>(opt => opt.UseSqlite("Data Source=/db/ActivityDb.db;"), ServiceLifetime.Singleton);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure app
 app.UseSoapEndpoint<IActivityService>("/Activities.asmx", new SoapEncoderOptions());
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
